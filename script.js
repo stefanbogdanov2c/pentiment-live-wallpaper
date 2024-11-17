9754cbf22dfe8889daf7920736a0a6078b1d5dcb
stefanbogdanov2c <sbogdanovic@synapticon.com>
2024-11-17 11:46:34 +0100

    Add parallax, improve rotating logic

--- script.js
+++ script.js
@@ -6,9 +6,15 @@ function rotateClock() {
   const minutes = now.getMinutes();
   const seconds = now.getSeconds();
   const totalDegrees = hours * 15 + minutes * 0.25 + seconds * 0.0041666666666667;
-  clock.style.transform = `rotate(${0-totalDegrees}deg)`;
+  clock.style.transform = `rotate(${0 - totalDegrees}deg)`;
 }
 
 setInterval(rotateClock, 10000);
 
 rotateClock();
+
+const parallaxInstance = new Parallax(document.querySelector('.container'));
+const parallaxGyro = new Parallax(document.querySelector('.container'), {
+  calibrateX: true,
+  calibrateY: true,
+});
