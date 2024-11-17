457c3234cc66e48bdf0fc39b55daf0aa337ca69f
stefanbogdanov2c <sbogdanovic@synapticon.com>
2024-11-17 10:39:02 +0100

    Add clock rotation logic, add background video

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-// alert('test')
+const clockImage = document.getElementById('clock');
+
+function rotateClock() {
+  const now = new Date();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+  console.log(hours);
+
+  // Calculate total degrees for the current time (24-hour clock, 360° rotation)
+  const totalDegrees = hours * 15 + minutes * 0.25;
+  // console.log(totalDegrees);
+  clock.style.transform = `rotate(${0-totalDegrees}deg)`;
+}
+
+setInterval(rotateClock, 60000);
+
+rotateClock();
